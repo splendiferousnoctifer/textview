1ac95967f7b3bd002dece128b6e35559e1c8a883
Samuel Zühlke <63741955+splendiferousnoctifer@users.noreply.github.com>
2026-02-04 16:21:42 +0100

    Initial commit: textarea.my static app with GitHub Pages deploy

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'textarea-2026-01-07'
+const ASSETS = [
+  '/',
+  '/qr',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    }).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request)
+      })
+  )
+})
